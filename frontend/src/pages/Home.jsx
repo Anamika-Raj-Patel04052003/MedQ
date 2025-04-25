9bef5e19703635ec0114ce5e0d9c7911b2c28e5c
Anamika-Raj-Patel04052003 <anamikaintern0304@gmail.com>
2025-04-26 00:37:55 +0530

    video lecture up to 1.11.00 min
    video lecture up to 1.11.00 min

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Header from '../components/Header'
+import specialityMenu from '../components/specialityMenu'
 const Home = () => {
   return (
     <div>
-      Homepage
+      <Header/>
+     <specialityMenu />
+
     </div>
   )
 }
